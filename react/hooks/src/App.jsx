73c783ef26f47e9a69f334679228ad7576007ec9
SaqibWani273 <saqibwani273@gmail.com>
2024-10-03 15:03:39 +0530

    use of wrapper components

--- react/hooks/src/App.jsx
+++ react/hooks/src/App.jsx
@@ -4,33 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
-  const todos=[
-    {id:1, title:"Todo 1",description:"Description 1"},
-    {id:2, title:"Todo 2",description:"Description 2"},
-    {id:3, title:"Todo 3",description:"Description 3"},
-  ];
-  const [todosState,setTodos]=useState(todos);
-  function addTodo({title,description,id}){
-    setTodos([{title:title,description:description,id:id},...todosState]);
+  function TextComponent(){
+    return <div>
+      <h1 >title 1</h1>
+      <p>description 1</p>
+    </div>
   }
-  return (
-      <div>
-      <button onClick={function temp(){
-        addTodo({title:"dummy title",description:"dummy description",id:4})
-      }}>Add Todo</button>
-      {todosState.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description}/>)}
-        </div>
-    
+  return(
+    <div>
+    <WrapperComponent >
+     
+      <div> hi there</div>
+      <TextComponent/>
+      
+    </WrapperComponent>
+    </div>
   )
- 
+  function WrapperComponent({children}){
+    console.log(children)
+    return <div style={{backgroundColor:'purple', border:'2px solid black', padding:30}}>
+{children}
+    </div>
+  }
 }
-function Todo({title,description}){
-  return <div>
-    <h1>{title}</h1>
-    
-    <h2>{description}</h2>
-  </div>
-}
+
 
 export default App
